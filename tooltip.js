f59d23b9ba1c60c0e38e8f527448e2c381ea2dc5
rocionadal <rocioonadal@gmail.com>
2021-04-02 13:23:08 +0200

    start with tooltip web c

--- tooltip.js
+++ tooltip.js
@@ -1,1 +1,14 @@
+class Tooltip extends HTMLElement {
+  constructor() {
+    super();
+  }
 
+  connectedCallback() { // DOM initializations
+    const tooltipIcon = document.createElement('span');
+    tooltipIcon.textContent = ' (?)';
+    tooltipIcon.addEventListener('mouseenter', )
+    this.appendChild(tooltipIcon);
+  }
+}
+
+customElements.define('rn-tooltip', Tooltip);
