4a69f210352ce3c1230341d1193423bf5dbe0af5
rocionadal <rocioonadal@gmail.com>
2021-04-02 13:40:39 +0200

    adding shadow root

--- tooltip.js
+++ tooltip.js
@@ -2,24 +2,29 @@ class Tooltip extends HTMLElement {
   constructor() {
     super();
     this._tooltipContainer;
+    this._tooltipText = 'Some default text.';
+    this.attachShadow({ mode: 'open' });
   }
-
+  
   connectedCallback() { // DOM initializations
+    if (this.hasAttribute('text')) {
+      this._tooltipText = this.getAttribute('text');
+    }
     const tooltipIcon = document.createElement('span');
     tooltipIcon.textContent = ' (?)';
     tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
     tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
-    this.appendChild(tooltipIcon);
+    this.shadowRoot.appendChild(tooltipIcon);
   }
 
   _showTooltip() { // to make clear that i will only call this method from inside the class
     this._tooltipContainer = document.createElement('div');
-    this._tooltipContainer.textContent = 'This is the tooltip text';
-    this.append(this._tooltipContainer);
+    this._tooltipContainer.textContent = this._tooltipText;
+    this.shadowRoot.append(this._tooltipContainer);
   }
 
   _hideTooltip() {
-    this.removeChild(this._tooltipContainer);
+    this.shadowRoot.removeChild(this._tooltipContainer);
 
   }
 }
