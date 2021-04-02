46c6c4633b90849c31ff51d9ca976e48f03aff8f
rocionadal <rocioonadal@gmail.com>
2021-04-02 13:28:32 +0200

    show and hide tooltip

--- tooltip.js
+++ tooltip.js
@@ -1,13 +1,26 @@
 class Tooltip extends HTMLElement {
   constructor() {
     super();
+    this._tooltipContainer;
   }
 
   connectedCallback() { // DOM initializations
     const tooltipIcon = document.createElement('span');
     tooltipIcon.textContent = ' (?)';
-    tooltipIcon.addEventListener('mouseenter', )
+    tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
+    tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
     this.appendChild(tooltipIcon);
+  }
+
+  _showTooltip() { // to make clear that i will only call this method from inside the class
+    this._tooltipContainer = document.createElement('div');
+    this._tooltipContainer.textContent = 'This is the tooltip text';
+    this.append(this._tooltipContainer);
+  }
+
+  _hideTooltip() {
+    this.removeChild(this._tooltipContainer);
+
   }
 }
 
